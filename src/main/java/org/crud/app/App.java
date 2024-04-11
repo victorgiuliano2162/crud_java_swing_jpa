b454faa0e2f03ddb3b9d65a638e6008f84856bb2
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-04-11 14:08:42 -0300

    applications execution add

--- src/main/java/org/crud/app/App.java
+++ src/main/java/org/crud/app/App.java
@@ -1,7 +1,12 @@
 package org.crud.app;
 
+
+import org.crud.swing.Frame;
+import org.crud.swing.Painel;
+
 public class App {
     public static void main(String[] args) {
 
+        new Painel();
     }
 }
