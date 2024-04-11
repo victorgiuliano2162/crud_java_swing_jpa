b032da5dc8c87b5296ecdf75b9252e119b20b137
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-04-11 14:07:27 -0300

    painel class creation white business rules

---
+++ src/main/java/org/crud/swing/Painel.java
@@ -0,0 +1,55 @@
+package org.crud.swing;
+
+import org.crud.infra.dao.IUserDAO;
+import org.crud.infra.dao.UserDAO;
+import org.crud.models.User;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Painel extends JPanel{
+
+    static final int SCREEN_WIDTH = 800;
+    static final int SCREEN_HEIGHT = 600;
+    IUserDAO userDAO = new UserDAO();
+
+    public Painel(){
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.white);
+        this.setFocusable(true);
+
+        startCrud(1);
+    }
+
+    public User tratarDados(String dados) {
+        if(dados == null) startCrud(0);
+        String[] dadosSeparados = dados.split(",");
+        if(dadosSeparados.length < 8 ) startCrud(2);
+        User user = new User();
+        return user;
+    }
+
+    public void startCrud(int a) {
+        String opcao = "";
+        if (a == 1) {
+        opcao = JOptionPane.showInputDialog(null,"Digite: \n1 para cadastro \n2 para consulta \n3 para exclusão \n4 para alteração \n5 para sair","Cadastro", JOptionPane.INFORMATION_MESSAGE);
+        } else if(a == 0) {
+            opcao = JOptionPane.showInputDialog(null,"Preencha os campos para efetuar o cadastro\nDigite: \n1 para cadastro \n2 para consulta \n3 para exclusão \n4 para alteração \n5 para sair","Cadastro", JOptionPane.INFORMATION_MESSAGE);
+        } else if(a == 2) {
+            opcao = JOptionPane.showInputDialog(null,"Os campos em branco devem constar como um espaço em branco entre vírgulas\nDigite: \n1 para cadastro \n2 para consulta \n3 para exclusão \n4 para alteração \n5 para sair","Cadastro", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        if(opcao.equals("1")){
+            String dados = JOptionPane.showInputDialog(null, "Digite os dados do cliente separados por vírgula, na seguinte ordem: nome, cpf, telefone, endereço, número, cidade, estado", "cadastro", JOptionPane.INFORMATION_MESSAGE);
+            tratarDados(dados);
+        } else if(opcao.equals("2")){
+
+        } else if(opcao.equals("3")){
+
+        } else if(opcao.equals("4")){
+
+        } else if(opcao.equals("5")){
+            System.exit(0);
+        }
+    }
+}
