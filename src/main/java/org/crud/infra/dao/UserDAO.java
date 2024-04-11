5bafde800d48fab8a504279de98ee9a997ae02d2
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-04-11 14:08:20 -0300

    CRUD method corretions

--- src/main/java/org/crud/infra/dao/UserDAO.java
+++ src/main/java/org/crud/infra/dao/UserDAO.java
@@ -2,9 +2,7 @@ package org.crud.infra.dao;
 
 import org.crud.models.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,30 +13,35 @@ public class UserDAO<E> implements IUserDAO {
 
     private static EntityManagerFactory emf;
     private static EntityManager em;
-    private Class<E> classe;
+    private Class<E> entityClass;
 
     static {
         try {
             emf = Persistence.createEntityManagerFactory("crud_java_swing_jpa");
         } catch (Exception e) {
-            System.out.println(e.getStackTrace());
+            e.getStackTrace();
         }
     }
 
-    public UserDAO() {
+    public void getMap() {
         this.map = new HashMap<>();
     }
+
     @Override
     public Boolean cadastrar(User u) {
+        abrirTransaction();
         if(this.map.containsKey(u.getCpf())){
             //Alterar pra JOptionPane
             System.out.println("Usuário já cadastrado");
             //Pensar na possibilidade de criar uma método que verifique as operação e retorne strings informado o estado das operações
+            fechar();
             return false;
         }
         this.map.put(u.getCpf(), u);
         //Alterar pra JOptionPane
         System.out.println("Usuário cadastrado com sucesso");
+        incluir((E) u);
+        fecharTransaction();
         return true;
     }
 
@@ -47,29 +50,75 @@ public class UserDAO<E> implements IUserDAO {
         User userCadastrado = this.map.get(cpf);
         if(userCadastrado != null) {
             this.map.remove(userCadastrado.getCpf(), userCadastrado);
+            excluirAtomico(userCadastrado);
         }
+        fechar();
     }
 
     @Override
     public void alterar(User u) {
+        abrirTransaction();
         User userCadastrado = this.map.get(u.getCpf());
-        if(userCadastrado != null){
+        if(obterPorID(u.getCpf()) != null){
             userCadastrado.setNome(u.getNome());
             userCadastrado.setTel(u.getTel());
             userCadastrado.setNumero(u.getNumero());
             userCadastrado.setEnd(u.getEnd());
             userCadastrado.setCidade(u.getCidade());
             userCadastrado.setEstado(u.getEstado());
+            incluir((E) u);
+            fecharTransaction();
+
         }
+        fechar();;
     }
 
     @Override
     public User consultar(Long cpf) {
+        if(obterPorID(cpf) != null) {
         return this.map.get(cpf);
+        }
+        return null;
     }
 
     @Override
     public Collection<User> buscarTodos() {
         return this.map.values();
+    }
+
+    public UserDAO<E> abrirTransaction() {
+        em.getTransaction().begin();
+        return this;
+    }
+
+    public UserDAO<E> fecharTransaction() {
+        em.getTransaction().commit();
+        return this;
+    }
+
+    public UserDAO<E> incluir(E e){
+        em.persist(e);
+        return this;
+    }
+
+    public UserDAO<E> incluirAtomico(E e){
+        return this.abrirTransaction().incluir(e).fecharTransaction();
+    }
+
+    public User obterPorID(Long cpf){
+        if(em.find(User.class, cpf) != null){
+        return em.find(User.class, cpf);
+        }
+        return null;
+    }
+
+    public void excluirAtomico(User u){
+        abrirTransaction();
+        em.remove(u);
+        fecharTransaction();
+    }
+
+    public void fechar(){
+        em.close();
     }
 }
