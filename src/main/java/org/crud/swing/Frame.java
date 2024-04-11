12dc88ce6427a23405452f9c571d346216cef5ef
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-04-11 14:06:53 -0300

    painel class creation

---
+++ src/main/java/org/crud/swing/Frame.java
@@ -0,0 +1,16 @@
+package org.crud.swing;
+
+import javax.swing.*;
+
+public class Frame extends JFrame {
+
+    public Frame() {
+        this.add(new Painel());
+        this.setTitle("Crud Java Swing");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(true);
+        this.pack();;
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
